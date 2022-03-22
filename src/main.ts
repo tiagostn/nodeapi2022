a5f92889c7aa70913c9e5997eae6bea1cc3de3c0
tiagostn <tiagostn@senado.leg.br>
2022-03-21 21:06:33 -0300

    adding dev script

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ info: 'OK!' });
+    return response.json({ info: 'OK!!!' });
 });
 
 app.listen(3030);
