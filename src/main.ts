1594a7160ea7d4dd83a5fd27d250933a08d9dbb4
tiagostn <tiagostn@senado.leg.br>
2022-03-21 21:46:18 -0300

    config paths - yarn add -D tsconfig-paths

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import UserService from "@services/UserService";
 
 const app = express();
 
 app.get('/', (request, response) => {
+    let userService = new UserService();
+    console.log(userService.name);
     return response.json({ info: 'OK!!!' });
 });
 
